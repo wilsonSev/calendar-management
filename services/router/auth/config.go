f09aaa2eef0cba02f5ebf5eef77d736c3ac5b907
Pribavkin Andrei Alexeevich <andrepribavkin@gmail.com>
2026-01-29 11:54:18 +0300

    Update

--- services/router/auth/config.go
+++ services/router/auth/config.go
@@ -1,1 +1,17 @@
 package auth
+
+import (
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var googleOAuthConfig = &oauth2.Config{
+	ClientID:     "YOUR_CLIENT_ID",
+	ClientSecret: "YOUR_CLIENT_SECRET",
+	RedirectURL:  "https://mydomain.com/oauth/google/callback",
+	Scopes: []string{
+		"https://www.googleapis.com/auth/calendar",
+		"https://www.googleapis.com/auth/tasks",
+	},
+	Endpoint: google.Endpoint,
+}
